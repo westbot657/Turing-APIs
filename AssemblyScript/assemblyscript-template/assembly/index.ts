0ce8435485a1aaeded6f538735225aa084c2b375
westbot <82739021+westbot657@users.noreply.github.com>
2025-04-13 04:22:47 -0600

    add assembly script template

---
+++ AssemblyScript/assemblyscript-template/assembly/index.ts
@@ -0,0 +1,13 @@
+// The entry file of your WebAssembly module.
+
+export function on_load(): void {
+    
+}
+
+export function on_update(beat: f32): void {
+
+}
+
+export function on_exit(): void {
+
+}
