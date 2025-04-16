63ca9850ed51848e753dff0a39f27bc5ac20f66e
westbot <82739021+westbot657@users.noreply.github.com>
2025-04-16 03:20:45 -0600

    some types are broken

--- AssemblyScript/assemblyscript-template/assembly/index.ts
+++ AssemblyScript/assemblyscript-template/assembly/index.ts
@@ -1,13 +1,13 @@
-import { TuringApi, Log, Beatmap } from "../../turing-api-assemblyscript/turing_api_assemblyscript";
+import * as Turing from "../../turing-api-assemblyscript/turing_api_assemblyscript";
 
 export function on_load(): void {
-    Log.info("AsmScript on_load!")
+    Turing.Log.info("AsmScript on_load!")
 }
 
 export function on_update(beat: f32): void {
-    let note = TuringApi.create_color_note(beat+2);
+    let note = Turing.create_color_note(beat+2);
 
-    Beatmap.add_color_note(note);
+    Turing.Beatmap.add_color_note(note);
 }
 
 export function on_exit(): void {
