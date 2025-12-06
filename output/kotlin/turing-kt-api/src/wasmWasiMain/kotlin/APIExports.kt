13e776246d9e3394b17ab994a54f3703e6f9bc64
westbot <82739021+westbot657@users.noreply.github.com>
2025-12-06 14:52:17 -0700

    fix WasmImport formatting and namespace

--- output/kotlin/turing-kt-api/src/wasmWasiMain/kotlin/APIExports.kt
+++ output/kotlin/turing-kt-api/src/wasmWasiMain/kotlin/APIExports.kt
@@ -9,28 +9,33 @@ import kotlinx.cinterop.*
 import kotlin.native.concurrent.*
 
 // host helper to copy string bytes from host memory into guest buffer
-@WasmImport("turing_api", "_host_strcpy")
+@WasmImport("env", "_host_strcpy")
 external fun _host_strcpy(location: CPointer<ByteVar>?, size: Int)
 
 // FFI imports for global functions
 
-@WasmImport("turing_api", "_test_global")
+@WasmImport("env", "_test_global")
 external fun _test_global(name: CPointer<ByteVar>?)
 
-@WasmImport("turing_api", "_global_2_test")
+@WasmImport("env", "_global_2_test")
 external fun _global_2_test(): Int
 
-@WasmImport("turing_api", "_my_test")
+@WasmImport("env", "_my_test")
 external fun _my_test(a: Byte, b: Short): CPointer<ByteVar>?
 
 
 // FFI imports for classes
-@WasmImport("turing_api", "_my_class_object_func")
-external fun _my_class_object_func(opaqu: Int, a: Short)@WasmImport("turing_api", "_log_info")
-external fun _log_info(msg: CPointer<ByteVar>?)@WasmImport("turing_api", "_log_warn")
-external fun _log_warn(msg: CPointer<ByteVar>?)@WasmImport("turing_api", "_log_critical")
-external fun _log_critical(msg: CPointer<ByteVar>?)@WasmImport("turing_api", "_log_debug")
+@WasmImport("env", "_my_class_object_func")
+external fun _my_class_object_func(opaqu: Int, a: Short)
+@WasmImport("env", "_log_info")
+external fun _log_info(msg: CPointer<ByteVar>?)
+@WasmImport("env", "_log_warn")
+external fun _log_warn(msg: CPointer<ByteVar>?)
+@WasmImport("env", "_log_critical")
+external fun _log_critical(msg: CPointer<ByteVar>?)
+@WasmImport("env", "_log_debug")
 external fun _log_debug(msg: CPointer<ByteVar>?)
+
 
 //// Functions ////
 
