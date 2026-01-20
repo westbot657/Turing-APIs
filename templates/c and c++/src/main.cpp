8b66677562b14cfe0ffe5d8215882df413770073
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2026-01-20 10:22:36 -0400

    Use GLM

--- templates/c and c++/src/main.cpp
+++ templates/c and c++/src/main.cpp
@@ -1,4 +1,13 @@
 #include "../api/turing_api_cpp.hpp"
+#include "glm/ext/vector_float2.hpp"
+
+
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/scalar_constants.hpp> // glm::pi
+#include <glm/mat4x4.hpp>               // glm::mat4
+#include <glm/vec3.hpp>                 // glm::vec3
+#include <glm/vec4.hpp>                 // glm::vec4
 
 /// this macro ensures the function is exported correctly in WebAssembly
 /// since Emscripten doesn't export extern "C" functions by default (it seems)
@@ -8,6 +17,12 @@
 extern "C" {
 
 WASM_EXPORT("on_load") void on_load() {
+  glm::vec2 v2(1.0f, 2.0f);
+  glm::vec3 v3(1.0f, 2.0f, 3.0f);
+
+  v2 = v2 + glm::vec2(3.0f, 4.0f);
+  v3 = v3 + glm::vec3(3.0f, 4.0f, 5.0f);
+
   // Called on map start
 }
 
