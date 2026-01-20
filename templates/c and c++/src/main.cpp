abf567baab728f8238f5d30c334fcb6c2a143b0d
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2026-01-20 08:24:50 -0400

    Fix symbol exporting and implement stripping

--- templates/c and c++/src/main.cpp
+++ templates/c and c++/src/main.cpp
@@ -1,21 +1,24 @@
 #include "../api/turing_api_cpp.hpp"
 
+/// this macro ensures the function is exported correctly in WebAssembly
+/// since Emscripten doesn't export extern "C" functions by default (it seems)
+/// - Fern
+#define WASM_EXPORT(name) __attribute__((visibility("default"))) __attribute__((used)) __attribute__((export_name(name)))
+
 extern "C" {
 
-void on_load() {
+WASM_EXPORT("on_load") void on_load() {
   // Called on map start
 }
 
-void on_update() {
+WASM_EXPORT("on_update") void on_update() {
   // Called every frame
 }
 
-void on_exit() {
+WASM_EXPORT("on_exit") void on_exit() {
   // Called on map fail/end/quit
 }
 
 // Add more hooks as needed
 
 } // extern "C"
-
-int main() { return 0; }
